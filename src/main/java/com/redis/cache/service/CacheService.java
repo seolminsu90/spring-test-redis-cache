e9a307d5817d9bc66b322021862fd5fae18bd170
seolminsu90 <53187393+seolminsu90@users.noreply.github.com>
2020-06-05 12:47:55 +0900

    Update CacheService.java
    나중에 볼때 직관적이게 이름 변경

--- src/main/java/com/redis/cache/service/CacheService.java
+++ src/main/java/com/redis/cache/service/CacheService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CacheService {
 
-    @Cacheable(value = "cache", keyGenerator = "keyGenerator")
+    @Cacheable(value = "cacheName", keyGenerator = "keyGenerator")
     public String cacheableTest(String normalKey) {
         try {
             Thread.sleep(5000);
@@ -19,7 +19,7 @@ public class CacheService {
         return "Hello world :: " + normalKey;
     }
 
-    @Cacheable(value = "cache", keyGenerator = "keyGenerator")
+    @Cacheable(value = "cacheName", keyGenerator = "keyGenerator")
     public String cacheableTest(Map<String, Object> params) {
         try {
             Thread.sleep(5000);
@@ -29,7 +29,7 @@ public class CacheService {
         return "Hello world :: " + params.toString();
     }
 
-    @Cacheable(value = "cache", keyGenerator = "keyGenerator")
+    @Cacheable(value = "cacheName", keyGenerator = "keyGenerator")
     public String cacheableTest(Map<String, Object> params, String normalKey) {
         try {
             Thread.sleep(5000);
@@ -39,7 +39,7 @@ public class CacheService {
         return "Hello world :: " + normalKey + " :: " + params.toString();
     }
 
-    @CacheEvict(value = "cache")
+    @CacheEvict(value = "cacheName")
     public String cacheableCleanAllTest() {
         return "ok";
     }
